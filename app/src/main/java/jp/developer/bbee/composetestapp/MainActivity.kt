a46ea517c87fb8e4115f98d9c02c20c85abb17f4
BBee <atsushi.kokubun.47@gmail.com>
2022-03-20 00:21:57 +0900

    ComposeTestApp: Add MessageCard

--- app/src/main/java/jp/developer/bbee/composetestapp/MainActivity.kt
+++ app/src/main/java/jp/developer/bbee/composetestapp/MainActivity.kt
@@ -3,41 +3,59 @@ package jp.developer.bbee.composetestapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import jp.developer.bbee.composetestapp.ui.theme.ComposeTestAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTestAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MessageCard(Message("Author", "Hoge hoge hoge"))
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(message: Message) {
+    Row (modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.bug_character_hachi_small),
+            contentDescription = "Java logo",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(0.4.dp, Color.LightGray, CircleShape)
+                .background(Color.Black, CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(10.dp))
+
+        Column {
+            Text(message.author)
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(message.body)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    ComposeTestAppTheme {
-        Greeting("Android")
-    }
+fun PreviewMessageCard() {
+    MessageCard(Message("Author", "Hoge hoge hoge"))
 }
